4e029ac325e527f9da321f7429514e129e39dceb
Tyson Phalp <tyson.phalp@mx.com>
2024-02-01 17:17:37 -0700

    Moved configs to root, and had to change some references to code

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:17
 
 WORKDIR /app
 
-COPY ./ ./
+COPY src ./
 
 RUN npm install 
 RUN npm install -g ts-node nodemon
--- start-docker.sh
+++ start-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 
-docker run -p8088:8080 -e Env=dev -e Port=8080 --env-file ${BASH_SOURCE%/*}/src/.env uvcs-demo
+docker run -p8088:8080 -e Env=dev -e Port=8080 --env-file ${BASH_SOURCE%/*}/.env uvcs-demo
